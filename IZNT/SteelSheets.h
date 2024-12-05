271a83b1b1651f8eb8940a8b22d59fcc93484cfc
Red-Sputnik <brodyaga.150304@mail.ru>
2024-12-05 11:26:15 +0300

    Fix SteelSheets

--- IZNT/SteelSheets.h
+++ IZNT/SteelSheets.h
@@ -51,14 +51,37 @@ namespace IZNT {
 
 
 
-	private: System::Windows::Forms::CheckBox^ checkArm;
-	private: System::Windows::Forms::CheckBox^ checkUnderwaterExp;
-	private: System::Windows::Forms::CheckBox^ checkDemolitionSkin;
+
+
+
+
+
+
 	private: System::Windows::Forms::ComboBox^ type_charge_comboBox;
 
 	private: System::Windows::Forms::Label^ label2;
-	private: System::Windows::Forms::Button^ btnWaterLeft;
-	private: System::Windows::Forms::Button^ btnWaterRight;
+
+
+
+
+
+
+
+
+	private: System::Windows::Forms::RadioButton^ checkUnderwaterExp;
+	private: System::Windows::Forms::RadioButton^ checkWoodenStop;
+	private: System::Windows::Forms::RadioButton^ checkDemolitionSkin;
+	private: System::Windows::Forms::RadioButton^ checkDemolitionSkinWater;
+	private: System::Windows::Forms::CheckBox^ checkArm;
+	private: System::Windows::Forms::RadioButton^ checkWater;
+	private: System::Windows::Forms::Label^ label3;
+
+
+
+
+
+
+
 
 
 
@@ -85,13 +108,15 @@ namespace IZNT {
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label4 = (gcnew System::Windows::Forms::Label());
 			this->width_steel_sheets_textBox = (gcnew System::Windows::Forms::TextBox());
-			this->checkArm = (gcnew System::Windows::Forms::CheckBox());
-			this->checkUnderwaterExp = (gcnew System::Windows::Forms::CheckBox());
-			this->checkDemolitionSkin = (gcnew System::Windows::Forms::CheckBox());
 			this->type_charge_comboBox = (gcnew System::Windows::Forms::ComboBox());
 			this->label2 = (gcnew System::Windows::Forms::Label());
-			this->btnWaterLeft = (gcnew System::Windows::Forms::Button());
-			this->btnWaterRight = (gcnew System::Windows::Forms::Button());
+			this->checkUnderwaterExp = (gcnew System::Windows::Forms::RadioButton());
+			this->checkWoodenStop = (gcnew System::Windows::Forms::RadioButton());
+			this->checkDemolitionSkin = (gcnew System::Windows::Forms::RadioButton());
+			this->checkDemolitionSkinWater = (gcnew System::Windows::Forms::RadioButton());
+			this->checkArm = (gcnew System::Windows::Forms::CheckBox());
+			this->checkWater = (gcnew System::Windows::Forms::RadioButton());
+			this->label3 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// label5
@@ -101,7 +126,7 @@ namespace IZNT {
 			this->label5->Location = System::Drawing::Point(13, 67);
 			this->label5->Margin = System::Windows::Forms::Padding(5, 0, 5, 0);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(136, 15);
+			this->label5->Size = System::Drawing::Size(202, 22);
 			this->label5->TabIndex = 23;
 			this->label5->Text = L"Толщина листа в см";
 			// 
@@ -111,7 +136,7 @@ namespace IZNT {
 			this->thickness_steel_sheets_textBox->Location = System::Drawing::Point(16, 85);
 			this->thickness_steel_sheets_textBox->Margin = System::Windows::Forms::Padding(5, 3, 5, 3);
 			this->thickness_steel_sheets_textBox->Name = L"thickness_steel_sheets_textBox";
-			this->thickness_steel_sheets_textBox->Size = System::Drawing::Size(211, 26);
+			this->thickness_steel_sheets_textBox->Size = System::Drawing::Size(211, 35);
 			this->thickness_steel_sheets_textBox->TabIndex = 22;
 			// 
 			// answer_textBox
@@ -148,7 +173,7 @@ namespace IZNT {
 			this->label1->Location = System::Drawing::Point(58, 21);
 			this->label1->Margin = System::Windows::Forms::Padding(5, 0, 5, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(593, 63);
+			this->label1->Size = System::Drawing::Size(877, 96);
 			this->label1->TabIndex = 19;
 			this->label1->Text = L"Расчет контактного заряда, необходимого для перебивания\r\nстального листа\r\n\r\n";
 			this->label1->TextAlign = System::Drawing::ContentAlignment::TopCenter;
@@ -160,7 +185,7 @@ namespace IZNT {
 			this->label4->Location = System::Drawing::Point(13, 139);
 			this->label4->Margin = System::Windows::Forms::Padding(5, 0, 5, 0);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(129, 15);
+			this->label4->Size = System::Drawing::Size(192, 22);
 			this->label4->TabIndex = 25;
 			this->label4->Text = L"Ширина листа в см";
 			// 
@@ -170,46 +195,8 @@ namespace IZNT {
 			this->width_steel_sheets_textBox->Location = System::Drawing::Point(16, 156);
 			this->width_steel_sheets_textBox->Margin = System::Windows::Forms::Padding(5, 3, 5, 3);
 			this->width_steel_sheets_textBox->Name = L"width_steel_sheets_textBox";
-			this->width_steel_sheets_textBox->Size = System::Drawing::Size(211, 26);
+			this->width_steel_sheets_textBox->Size = System::Drawing::Size(211, 35);
 			this->width_steel_sheets_textBox->TabIndex = 24;
-			// 
-			// checkArm
-			// 
-			this->checkArm->AutoSize = true;
-			this->checkArm->Font = (gcnew System::Drawing::Font(L"Lucida Sans", 12.25F));
-			this->checkArm->Location = System::Drawing::Point(16, 268);
-			this->checkArm->Margin = System::Windows::Forms::Padding(4, 3, 4, 3);
-			this->checkArm->Name = L"checkArm";
-			this->checkArm->Size = System::Drawing::Size(152, 24);
-			this->checkArm->TabIndex = 28;
-			this->checkArm->Text = L"Броневой лист";
-			this->checkArm->UseVisualStyleBackColor = true;
-			// 
-			// checkUnderwaterExp
-			// 
-			this->checkUnderwaterExp->AutoSize = true;
-			this->checkUnderwaterExp->Font = (gcnew System::Drawing::Font(L"Lucida Sans", 12.25F));
-			this->checkUnderwaterExp->Location = System::Drawing::Point(16, 309);
-			this->checkUnderwaterExp->Margin = System::Windows::Forms::Padding(4, 3, 4, 3);
-			this->checkUnderwaterExp->Name = L"checkUnderwaterExp";
-			this->checkUnderwaterExp->Size = System::Drawing::Size(187, 24);
-			this->checkUnderwaterExp->TabIndex = 29;
-			this->checkUnderwaterExp->Text = L"Подрыв под водой";
-			this->checkUnderwaterExp->UseVisualStyleBackColor = true;
-			this->checkUnderwaterExp->CheckedChanged += gcnew System::EventHandler(this, &SteelSheets::checkUnderwaterExp_CheckedChanged);
-			// 
-			// checkDemolitionSkin
-			// 
-			this->checkDemolitionSkin->AutoSize = true;
-			this->checkDemolitionSkin->Font = (gcnew System::Drawing::Font(L"Lucida Sans", 12.25F));
-			this->checkDemolitionSkin->Location = System::Drawing::Point(16, 445);
-			this->checkDemolitionSkin->Margin = System::Windows::Forms::Padding(4, 3, 4, 3);
-			this->checkDemolitionSkin->Name = L"checkDemolitionSkin";
-			this->checkDemolitionSkin->Size = System::Drawing::Size(176, 44);
-			this->checkDemolitionSkin->TabIndex = 30;
-			this->checkDemolitionSkin->Text = L"Подрыв стальной\r\nобшивки судна\r\n";
-			this->checkDemolitionSkin->UseVisualStyleBackColor = true;
-			this->checkDemolitionSkin->CheckedChanged += gcnew System::EventHandler(this, &SteelSheets::checkDemolitionSkin_CheckedChanged);
 			// 
 			// type_charge_comboBox
 			// 
@@ -225,7 +212,7 @@ namespace IZNT {
 			this->type_charge_comboBox->Margin = System::Windows::Forms::Padding(4, 3, 4, 3);
 			this->type_charge_comboBox->Name = L"type_charge_comboBox";
 			this->type_charge_comboBox->RightToLeft = System::Windows::Forms::RightToLeft::No;
-			this->type_charge_comboBox->Size = System::Drawing::Size(211, 28);
+			this->type_charge_comboBox->Size = System::Drawing::Size(211, 37);
 			this->type_charge_comboBox->TabIndex = 31;
 			// 
 			// label2
@@ -235,45 +222,106 @@ namespace IZNT {
 			this->label2->Location = System::Drawing::Point(13, 201);
 			this->label2->Margin = System::Windows::Forms::Padding(5, 0, 5, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(79, 15);
+			this->label2->Size = System::Drawing::Size(118, 22);
 			this->label2->TabIndex = 33;
 			this->label2->Text = L"Тип заряда";
 			// 
-			// btnWaterLeft
+			// checkUnderwaterExp
 			// 
-			this->btnWaterLeft->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->btnWaterLeft->Location = System::Drawing::Point(17, 339);
-			this->btnWaterLeft->Name = L"btnWaterLeft";
-			this->btnWaterLeft->Size = System::Drawing::Size(75, 100);
-			this->btnWaterLeft->TabIndex = 34;
-			this->btnWaterLeft->Text = L" ";
-			this->btnWaterLeft->UseVisualStyleBackColor = true;
-			this->btnWaterLeft->Click += gcnew System::EventHandler(this, &SteelSheets::btnWaterLeft_Click);
+			this->checkUnderwaterExp->AutoSize = true;
+			this->checkUnderwaterExp->Font = (gcnew System::Drawing::Font(L"Lucida Sans", 12.25F));
+			this->checkUnderwaterExp->Location = System::Drawing::Point(14, 419);
+			this->checkUnderwaterExp->Name = L"checkUnderwaterExp";
+			this->checkUnderwaterExp->Size = System::Drawing::Size(454, 33);
+			this->checkUnderwaterExp->TabIndex = 39;
+			this->checkUnderwaterExp->TabStop = true;
+			this->checkUnderwaterExp->Text = L"Под водой (вода с обеих сторон)";
+			this->checkUnderwaterExp->UseVisualStyleBackColor = true;
 			// 
-			// btnWaterRight
+			// checkWoodenStop
 			// 
-			this->btnWaterRight->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->btnWaterRight->Location = System::Drawing::Point(98, 339);
-			this->btnWaterRight->Name = L"btnWaterRight";
-			this->btnWaterRight->Size = System::Drawing::Size(75, 100);
-			this->btnWaterRight->TabIndex = 35;
-			this->btnWaterRight->Text = L" ";
-			this->btnWaterRight->UseVisualStyleBackColor = true;
-			this->btnWaterRight->Click += gcnew System::EventHandler(this, &SteelSheets::btnWaterRight_Click);
+			this->checkWoodenStop->AutoSize = true;
+			this->checkWoodenStop->Font = (gcnew System::Drawing::Font(L"Lucida Sans", 12.25F));
+			this->checkWoodenStop->Location = System::Drawing::Point(15, 458);
+			this->checkWoodenStop->Name = L"checkWoodenStop";
+			this->checkWoodenStop->Size = System::Drawing::Size(430, 33);
+			this->checkWoodenStop->TabIndex = 40;
+			this->checkWoodenStop->TabStop = true;
+			this->checkWoodenStop->Text = L"Вода с обеих сторон с бруском";
+			this->checkWoodenStop->UseVisualStyleBackColor = true;
+			// 
+			// checkDemolitionSkin
+			// 
+			this->checkDemolitionSkin->AutoSize = true;
+			this->checkDemolitionSkin->Font = (gcnew System::Drawing::Font(L"Lucida Sans", 12.25F));
+			this->checkDemolitionSkin->Location = System::Drawing::Point(15, 497);
+			this->checkDemolitionSkin->Name = L"checkDemolitionSkin";
+			this->checkDemolitionSkin->Size = System::Drawing::Size(341, 33);
+			this->checkDemolitionSkin->TabIndex = 41;
+			this->checkDemolitionSkin->TabStop = true;
+			this->checkDemolitionSkin->Text = L"Вода со стороны заряда";
+			this->checkDemolitionSkin->UseVisualStyleBackColor = true;
+			// 
+			// checkDemolitionSkinWater
+			// 
+			this->checkDemolitionSkinWater->AutoSize = true;
+			this->checkDemolitionSkinWater->Font = (gcnew System::Drawing::Font(L"Lucida Sans", 12.25F));
+			this->checkDemolitionSkinWater->Location = System::Drawing::Point(15, 536);
+			this->checkDemolitionSkinWater->Name = L"checkDemolitionSkinWater";
+			this->checkDemolitionSkinWater->Size = System::Drawing::Size(585, 33);
+			this->checkDemolitionSkinWater->TabIndex = 42;
+			this->checkDemolitionSkinWater->TabStop = true;
+			this->checkDemolitionSkinWater->Text = L"Вода со стороны, противоположной заряду";
+			this->checkDemolitionSkinWater->UseVisualStyleBackColor = true;
+			// 
+			// checkArm
+			// 
+			this->checkArm->AutoSize = true;
+			this->checkArm->Font = (gcnew System::Drawing::Font(L"Lucida Sans", 12.25F));
+			this->checkArm->Location = System::Drawing::Point(14, 288);
+			this->checkArm->Name = L"checkArm";
+			this->checkArm->Size = System::Drawing::Size(223, 33);
+			this->checkArm->TabIndex = 43;
+			this->checkArm->Text = L"Броневой лист";
+			this->checkArm->UseVisualStyleBackColor = true;
+			// 
+			// checkWater
+			// 
+			this->checkWater->AutoSize = true;
+			this->checkWater->Font = (gcnew System::Drawing::Font(L"Lucida Sans", 12.25F));
+			this->checkWater->Location = System::Drawing::Point(14, 380);
+			this->checkWater->Name = L"checkWater";
+			this->checkWater->Size = System::Drawing::Size(158, 33);
+			this->checkWater->TabIndex = 44;
+			this->checkWater->TabStop = true;
+			this->checkWater->Text = L"Не в воде";
+			this->checkWater->UseVisualStyleBackColor = true;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Lucida Sans", 14.25F, System::Drawing::FontStyle::Bold));
+			this->label3->Location = System::Drawing::Point(10, 336);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(252, 34);
+			this->label3->TabIndex = 45;
+			this->label3->Text = L"Место подрыва";
 			// 
 			// SteelSheets
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(7, 12);
+			this->AutoScaleDimensions = System::Drawing::SizeF(10, 20);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(64)), static_cast<System::Int32>(static_cast<System::Byte>(64)),
 				static_cast<System::Int32>(static_cast<System::Byte>(64)));
-			this->Controls->Add(this->btnWaterRight);
-			this->Controls->Add(this->btnWaterLeft);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->checkWater);
+			this->Controls->Add(this->checkArm);
+			this->Controls->Add(this->checkDemolitionSkinWater);
+			this->Controls->Add(this->checkDemolitionSkin);
+			this->Controls->Add(this->checkWoodenStop);
+			this->Controls->Add(this->checkUnderwaterExp);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->type_charge_comboBox);
-			this->Controls->Add(this->checkDemolitionSkin);
-			this->Controls->Add(this->checkUnderwaterExp);
-			this->Controls->Add(this->checkArm);
 			this->Controls->Add(this->label4);
 			this->Controls->Add(this->width_steel_sheets_textBox);
 			this->Controls->Add(this->label5);
@@ -286,15 +334,14 @@ namespace IZNT {
 			this->Margin = System::Windows::Forms::Padding(4, 3, 4, 3);
 			this->Name = L"SteelSheets";
 			this->Size = System::Drawing::Size(719, 591);
+			this->Load += gcnew System::EventHandler(this, &SteelSheets::SteelSheets_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void calc_button_Click(System::Object^ sender, System::EventArgs^ e);
-	private: System::Void checkUnderwaterExp_CheckedChanged(System::Object^ sender, System::EventArgs^ e);
-	private: System::Void checkDemolitionSkin_CheckedChanged(System::Object^ sender, System::EventArgs^ e);
-	private: System::Void btnWaterLeft_Click(System::Object^ sender, System::EventArgs^ e);
-	private: System::Void btnWaterRight_Click(System::Object^ sender, System::EventArgs^ e);
+private: System::Void SteelSheets_Load(System::Object^ sender, System::EventArgs^ e) {
+}
 };
 }
